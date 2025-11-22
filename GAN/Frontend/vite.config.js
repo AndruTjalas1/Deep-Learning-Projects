c2684fa747a5786cfe69eae104928dd809ad9959
TatumHansen19 <tatumhansen19@gmail.com>
2025-11-22 12:19:26 -0700

    GAN frontend + backend communcation attempt fixes

--- GAN/Frontend/vite.config.js
+++ GAN/Frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/gan/",
+
   server: {
     port: 3000,
     proxy: {
@@ -13,6 +16,7 @@ export default defineConfig({
       },
     },
   },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
