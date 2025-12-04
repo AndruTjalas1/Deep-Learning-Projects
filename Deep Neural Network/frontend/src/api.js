3b84ab79ee87c2a22c9bd168701777be964823c0
TatumHansen19 <tatumhansen19@gmail.com>
2025-12-03 23:15:30 -0700

    Update api.js

--- Deep Neural Network/frontend/src/api.js
+++ Deep Neural Network/frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL3 || 'http://localhost:8002'
+const API_URL = import.meta.env.VITE_API_URL3 || 'https://localhost:8002'
 
 const api = axios.create({
   baseURL: API_URL,
